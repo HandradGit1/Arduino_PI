72ca18a9165f7a67aed2a68b1ecde62f0f7a5d11
HandradGit1 <116686731+HandradGit1@users.noreply.github.com>
2022-10-26 20:01:04 -0300

    Versão Funcional 1.0
    v1.0 Funcional

---
+++ Aduinov2.ino
@@ -0,0 +1,30 @@
+const char ABRIR_PORTA = 'A';
+const char FECHAR_PORTA = 'B';
+const int PORTA_LED = 13;
+char cobaia =' ';
+
+void setup()
+{
+  pinMode(PORTA_LED,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop(){
+  
+  if(Serial.available()>0){
+    cobaia = Serial.read();
+ 
+ switch(cobaia){
+  case ABRIR_PORTA:
+      digitalWrite(13,HIGH);
+      Serial.print("ABERTO");
+      digitalWrite(13,LOW);
+      break;
+  case FECHAR_PORTA:
+      digitalWrite(13,HIGH);
+      Serial.print("FECHADO");
+      digitalWrite(13,LOW);
+      break;
+  };
+ }
+}
